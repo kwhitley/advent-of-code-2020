fc14d42ffe4227a13aaf5b6b5075e53873ca541a
Kevin Whitley <krwhitley@gmail.com>
2020-12-02 13:22:17 -0600

    cleanup

--- src/day2/day2.js
+++ src/day2/day2.js
@@ -1,19 +1,15 @@
 const oldPolicy = ({ a, b, target, password }) => {
   const matches = password.replace(new RegExp(`[^${target}]`, 'g'), '').length
-  return matches >= Number(a) && matches <= Number(b)
+  return matches >= +a && matches <= +b
 }
 
 const newPolicy = ({ a, b, target, password }) =>
   ((password[a - 1] === target) + (password[b - 1] === target)) === 1
 
-const validPasswords = (
-  passwords = [],
-  policy = oldPolicy,
-) => passwords.reduce((acc, entry) => {
-  const passwordParts = entry.match(/^(?<a>\d+)-(?<b>\d+)\s+(?<target>\w+).*?(?<password>\w+)$/).groups
-
-  return acc += policy(passwordParts) ? 1 : 0
-}, 0)
+const validPasswords = (passwords, policy) =>
+  passwords.reduce((acc, entry) =>
+    acc += policy(entry.match(/^(?<a>\d+)-(?<b>\d+)\s+(?<target>\w+).*?(?<password>\w+)$/).groups) ? 1 : 0
+  , 0)
 
 module.exports = {
   validPasswords,
--- src/day2/day2.spec.js
+++ src/day2/day2.spec.js
@@ -1,16 +1,16 @@
-const { validPasswords, newPolicy } = require('./day2')
+const { validPasswords, newPolicy, oldPolicy } = require('./day2')
 const values = require('./values').split('\n')
 
 describe('validPasswords', () => {
   it('finds 2 valid in test set', () => {
     const example = ['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']
-    expect(validPasswords(example)).toBe(2)
+    expect(validPasswords(example, oldPolicy)).toBe(2)
   })
 })
 
 describe('ANSWER', () => {
   it('part1 === 467', () => {
-    expect(validPasswords(values)).toBe(467)
+    expect(validPasswords(values, oldPolicy)).toBe(467)
   })
 
   it('part2 === 59885340', () => {
